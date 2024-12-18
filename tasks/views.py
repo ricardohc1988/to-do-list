a3b5b4d656c7313f8eee67fb3ec8731ac6c2d002
Ricardo Huerta <ricardohc1988@gmail.com>
2024-12-18 14:45:46 -0600

    Implement views and serializers for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class TaskListCreateAPIView(generics.ListCreateAPIView):
+    # Permite obtener solo las tareas del usuario autenticado
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Solo obtiene tareas del usuario autenticado
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Asocia la tarea al usuario autenticado
+        serializer.save(user=self.request.user)
